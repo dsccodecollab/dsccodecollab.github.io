65cc3ce72db77814878194c0675634b81590812c
Bestin <bestinbthomas@gmail.com>
2020-10-01 15:29:02 +0530

    add boiler plate code

---
+++ src/tests/App.test.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+import App from '../components/App'
+import '@testing-library/jest-dom/extend-expect'
+
+test('renders learn react link', () => {
+  const { getByText } = render(<App />)
+  const linkElement = getByText(/Welcome to Code Collab/i)
+  expect(linkElement).toBeInTheDocument()
+})
---
+++ src/types/ExampleType.ts
@@ -0,0 +1,10 @@
+// just for reference
+
+export default interface ExampleType {
+    id: number,
+    name: string,
+    obj: {
+        num: number,
+        somethings: string[]
+    }[] // array
+}
