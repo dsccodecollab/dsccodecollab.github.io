d00dae72082f80ac5c2cf1ec9bab118e40fc6b8c
Bestin <bestinbthomas@gmail.com>
2020-10-03 19:18:54 +0530

    add firebase

---
+++ src/config.ts
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyA-VHOgEdOj10lD2jTiwehnHFsSoLV76yY',
+  authDomain: 'dsc-code-collab.firebaseapp.com',
+  databaseURL: 'https://dsc-code-collab.firebaseio.com',
+  projectId: 'dsc-code-collab',
+  storageBucket: 'dsc-code-collab.appspot.com',
+  messagingSenderId: '76652849293',
+  appId: '1:76652849293:web:7c7ff378dc1e69bf241ac4',
+  measurementId: 'G-CLDVE6SJD2'
+}
