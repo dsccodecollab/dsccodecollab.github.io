6c224799effafd11e74a2fbfdbe307b9cfe7e546
Bestin B Thomas <Bestinbthomas@gmail.com>
2020-10-04 23:14:41 +0530

    add firebase (#2)

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './components/App'
 import * as serviceWorker from './serviceWorker'
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import { firebaseConfig } from './config'
 
 ReactDOM.render(
   <React.StrictMode>
@@ -15,3 +18,4 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.register()
+firebase.initializeApp(firebaseConfig)
