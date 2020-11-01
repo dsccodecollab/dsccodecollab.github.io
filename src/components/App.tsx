e975efbbef528365a4a58523528ec86634906673
Avisek Shaw <56290578+shawavisek35@users.noreply.github.com>
2020-11-01 10:10:08 +0530

    Shawavisek35 branch (#4)
    * Navbar modified :smile: | minor bug fixes | Dependencies added
    
    * Navbar modified :smile: | minor bug fixes | Dependencies added
    
    * bug fixes

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,33 +1,38 @@
 import React from 'react'
-import Component1 from './compenent1'
-import ExampleType from '../types/ExampleType'
+// import Component1 from './compenent1'
+// import ExampleType from '../types/ExampleType'
+import Header from './Header'
 import '../styles/App.css'
 
 const App = () => {
-  const prop: ExampleType = {
-    id: 12345,
-    name: 'some name',
-    obj: [
-      {
-        num: 1,
-        somethings: ['a', 'b', 'c']
-      },
-      {
-        num: 2,
-        somethings: ['d', 'e', 'f']
-      },
-      {
-        num: 3,
-        somethings: ['g', 'h', 'i']
-      }
-    ]
-  }
+  // const prop: ExampleType = {
+  //   id: 12345,
+  //   name: 'some name',
+  //   obj: [
+  //     {
+  //       num: 1,
+  //       somethings: ['a', 'b', 'c']
+  //     },
+  //     {
+  //       num: 2,
+  //       somethings: ['d', 'e', 'f']
+  //     },
+  //     {
+  //       num: 3,
+  //       somethings: ['g', 'h', 'i']
+  //     }
+  //   ]
+  // }
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Welcome to Code Collab</p>
-      </header>
-      <Component1 arg={prop} />
+    <div
+      className="container-fluid"
+      style={{ width: '95%' }}
+    >
+      <Header />
+      <div className="jumbotron text-center mt-4 welcome-tab">
+        <p className="welcome">Welcome to The Code Culture</p>
+        <h1 className="brand-heading">Talk is cheap, show me the code</h1>
+      </div>
     </div>
   )
 }
