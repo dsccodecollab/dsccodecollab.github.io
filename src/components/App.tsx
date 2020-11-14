8ec0425ff0fa33c157b7071dc4ad536c1979633e
SphericalKat <amolele@gmail.com>
2020-11-14 13:52:10 +0530

    style(treewide): remove particles
    Signed-off-by: SphericalKat <amolele@gmail.com>

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -11,7 +11,6 @@ import {
 } from 'react-router-dom'
 import Topic from './Topic'
 import { ToastContainer } from 'react-toastify'
-import Particles from 'react-particles-js'
 
 const App = () => {
   // const [user, setUser] = useState({})
@@ -76,122 +75,6 @@ const App = () => {
           }
           <Redirect to="/" />
         </Switch>
-        <Particles
-          params={{
-            particles: {
-              number: {
-                value: 60,
-                density: {
-                  enable: true,
-                  value_area: 800
-                }
-              },
-              color: {
-                value: '#ffff00'
-              },
-              shape: {
-                type: 'circle',
-                stroke: {
-                  width: 0,
-                  color: '#000000'
-                },
-                polygon: {
-                  nb_sides: 5
-                }
-              },
-              opacity: {
-                value: 0.5,
-                random: false,
-                anim: {
-                  enable: false,
-                  speed: 1,
-                  opacity_min: 0.1,
-                  sync: false
-                }
-              },
-              size: {
-                value: 10,
-                random: true,
-                anim: {
-                  enable: false,
-                  speed: 80,
-                  size_min: 0.1,
-                  sync: false
-                }
-              },
-              line_linked: {
-                enable: true,
-                distance: 300,
-                color: '#ffffff',
-                opacity: 0.4,
-                width: 2
-              },
-              move: {
-                enable: true,
-                speed: 12,
-                direction: 'none',
-                random: false,
-                straight: false,
-                out_mode: 'out',
-                bounce: false,
-                attract: {
-                  enable: false,
-                  rotateX: 600,
-                  rotateY: 1200
-                }
-              }
-            },
-            interactivity: {
-              detect_on: 'canvas',
-              events: {
-                onhover: {
-                  enable: false,
-                  mode: 'repulse'
-                },
-                onclick: {
-                  enable: true,
-                  mode: 'push'
-                },
-                resize: true
-              },
-              modes: {
-                grab: {
-                  distance: 800,
-                  line_linked: {
-                    opacity: 1
-                  }
-                },
-                bubble: {
-                  distance: 800,
-                  size: 80,
-                  duration: 2,
-                  opacity: 0.8
-                },
-                repulse: {
-                  distance: 400,
-                  duration: 0.4
-                },
-                push: {
-                  particles_nb: 4
-                },
-                remove: {
-                  particles_nb: 2
-                }
-              }
-            },
-            retina_detect: true
-          }}
-          style={{
-            width: '1000vh',
-            height: '100%',
-            position: 'fixed',
-            margin: 0,
-            padding: 0,
-            top: 0,
-            left: 0,
-            zIndex: -999
-          }}
-        />
       </div>
     </BrowserRouter>
   )
