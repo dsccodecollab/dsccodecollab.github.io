4c00658d3bb1354b6ac4ac2f77a96ce3f258df07
Mit-Patel <patelmit2012@gmail.com>
2020-11-17 15:57:32 +0530

    fix errors

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -35,28 +35,28 @@ const Footer = () => {
               <button
                 className="bg-white text-blue-400 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2 p-0 flex"
                 type="button"
-                onClick={ (e) => window.open('https://www.google.com') }
+                onClick={ (e) => window.open('/') }
               >
                 <i className="flex fab fa-twitter"></i>
               </button>
               <button
                 className="bg-white text-blue-600 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2 p-0 flex"
                 type="button"
-                onClick={ (e) => window.open('https://www.google.com') }
+                onClick={ (e) => window.open('/') }
               >
                 <i className="flex fab fa-facebook-square"></i>
               </button>
               <button
                 className="bg-white text-pink-400 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2 p-0 flex"
                 type="button"
-                onClick={ (e) => window.open('https://www.google.com') }
+                onClick={ (e) => window.open('/') }
               >
                 <i className="flex fab fa-instagram"></i>
               </button>
               <button
                 className="bg-white text-gray-900 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2 p-0 flex"
                 type="button"
-                onClick={ (e) => window.open('https://www.google.com') }
+                onClick={ (e) => window.open('/') }
               >
                 <i className="flex fab fa-github"></i>
               </button>
@@ -71,17 +71,17 @@ const Footer = () => {
                 <ul className="list-unstyled">
                   <li>
                     <a className="text-gray-700 hover:text-gray-900 font-semibold block pb-2 text-sm"
-                      href="#">About Us
+                      href="/">About Us
                     </a>
                   </li>
                   <li>
                     <a className="text-gray-700 hover:text-gray-900 font-semibold block pb-2 text-sm"
-                      href="#">Community Partners
+                      href="/">Community Partners
                     </a>
                   </li>
                   <li>
                     <a className="text-gray-700 hover:text-gray-900 font-semibold block pb-2 text-sm"
-                      href="#">Sponsers
+                      href="/">Sponsers
                     </a>
                   </li>
                 </ul>
@@ -93,17 +93,17 @@ const Footer = () => {
                 <ul className="list-unstyled">
                   <li>
                     <a className="text-gray-700 hover:text-gray-900 font-semibold block pb-2 text-sm"
-                      href="#">Terms & Conditions
+                      href="/">Terms & Conditions
                     </a>
                   </li>
                   <li>
                     <a className="text-gray-700 hover:text-gray-900 font-semibold block pb-2 text-sm"
-                      href="#">Privacy Policy
+                      href="/">Privacy Policy
                     </a>
                   </li>
                   <li>
                     <a className="text-gray-700 hover:text-gray-900 font-semibold block pb-2 text-sm"
-                      href="#">Contact Us
+                      href="/">Contact Us
                     </a>
                   </li>
                 </ul>
