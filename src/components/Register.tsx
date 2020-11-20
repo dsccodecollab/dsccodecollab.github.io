d9f95d0355f364cc3d9b86a904e25f19669bfce9
Avisek Shaw <56290578+shawavisek35@users.noreply.github.com>
2020-11-20 14:54:58 +0530

    Update Register.tsx

--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -54,7 +54,7 @@ const Register = () => {
                 name: '',
                 universityName: ''
               })
-              toast.success('Registered for the upcoming contest. Check your email for more details.')
+              toast.success('Thanks for subscribing. We will send you an email shortly with further details')
             })
             .catch((err) => {
               setUser({
